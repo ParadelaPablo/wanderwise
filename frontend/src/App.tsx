d0e7d938e15f538503a3a4714da9c4b3cf3092d7
Pablo Paradela <pablo.paradela@appliedtechnology.se>
2024-11-25 16:21:17 +0100

    erase components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,7 @@ import { Providers } from "./utils/providers";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import './index.css'
 import { routeTree } from "./routeTree.gen";
-import Footer from "./components/footer"; 
-import HomeBeforeLogIn from "./components/homeBeforeLogIn";
-import HamburguerMenu from "./components/hamburguerMenu";
+
 
 
 const router = createRouter({ routeTree });
@@ -17,9 +15,6 @@ function App() {
   return (
     <Providers>
       <RouterProvider router={router} />
-      <HamburguerMenu />
-      <HomeBeforeLogIn />
-      <Footer />
     </Providers>
   );
 }
