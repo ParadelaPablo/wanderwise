efee0cab442e5052c620626ab7c84f9a2f110e5f
Habib Hezarehee <hezarehee.h@gmail.com>
2024-11-25 09:53:40 +0100

    Adds backend

---
+++ backend/src/main/java/org/wanderwise/wanderwise/WanderwiseApplication.java
@@ -0,0 +1,13 @@
+package org.wanderwise.wanderwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanderwiseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/wanderwise/wanderwise/WanderwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wanderwise.wanderwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
