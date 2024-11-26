afdd288e8962efeeddd2c0f59eb4958ab5243d82
Habib Hezarehee <hezarehee.h@gmail.com>
2024-11-26 14:16:15 +0100

    Resolves errors regarding to controller and JPA

--- backend/src/main/java/org/wanderwise/wanderwise/repository/UserTripRepository.java
+++ backend/src/main/java/org/wanderwise/wanderwise/repository/UserTripRepository.java
@@ -2,7 +2,8 @@ package org.wanderwise.wanderwise.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.wanderwise.wanderwise.entity.UserTrip;
 
 @Repository
-public interface UserTripRepository extends JpaRepository<UserTripRepository, Long> {
+public interface UserTripRepository extends JpaRepository<UserTrip, Long> {
 }
