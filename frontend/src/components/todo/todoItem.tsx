f948beb949cee53d8066dd10dc38dbbc7ee42d47
Sebastian Österberg <sebastian.osterberg@appliedtechnology.se>
2024-11-26 16:23:48 +0100

    Updated topack and todo with new styles and formatting

--- frontend/src/components/todo/todoItem.tsx
+++ frontend/src/components/todo/todoItem.tsx
@@ -1,9 +1,9 @@
 const TodoItem = () => {
   return (
     <div className="form-control">
-      <label className="label cursor-pointer">
-        <input type="checkbox" defaultChecked className="checkbox" />
-        <span className="label-text ml-2">Todo item</span>
+      <label className="label cursor-pointer justify-start">
+        <input type="checkbox" className="checkbox" />
+        <input type="text" placeholder="Item..." className="input input-bordered w-full max-w-xs h-8 ml-2" />
       </label>
     </div>
   );
