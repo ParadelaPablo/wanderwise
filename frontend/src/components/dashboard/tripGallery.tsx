b360dc75a08e8b5655fa4c4d2a95e7516fb3c44b
Pablo Paradela <pablo.paradela@appliedtechnology.se>
2024-11-27 10:55:03 +0100

    welcoming to the user, settings from clerk in dashboard/settings and habemus logo in the hamburguer menu

--- frontend/src/components/dashboard/tripGallery.tsx
+++ frontend/src/components/dashboard/tripGallery.tsx
@@ -1,14 +1,30 @@
 import TripCard from "./tripCard";
 import ButtonCircle from "../buttons/buttonAddNewTripCircle";
-import { useRouter } from "@tanstack/react-router"; // Cambiamos a useRouter
+import { useRouter } from "@tanstack/react-router";
+import { useUser } from "@clerk/clerk-react";
+
 
 const TripGallery = () => {
-  const router = useRouter(); 
+  const router = useRouter();
+  const { user } = useUser();
+
+  const trips = [
+    { id: 1, title: "Trip to Milano" },
+    { id: 2, title: "Trip to Roma" },
+    { id: 3, title: "Trip to Firenze" },
+    { id: 4, title: "Trip to Napoli" },
+    { id: 5, title: "Trip to Venezia" },
+    { id: 6, title: "Trip to Milano" },
+
+  ];
 
   return (
     <div className="flex flex-col items-center justify-between min-h-screen relative">
       <div className="text-center mt-5">
-        <h1 className="text-2xl font-bold">Welcome to Your Dashboard</h1>
+      <h1 className="text-2xl font-bold">
+          Welcome to Your Dashboard
+          {user?.firstName && `, ${user.firstName}`} 
+        </h1>
         <button
           onClick={() => router.navigate({ to: "/dashboard/create" })}
           className="bg-blue-500 text-white px-4 py-2 mt-4 rounded hover:bg-blue-600"
@@ -18,15 +34,13 @@ const TripGallery = () => {
       </div>
 
       <div className="flex flex-col w-80 items-center justify-center gap-5 mt-8 mb-20 border p-5">
-        <TripCard />
-        <TripCard />
-        <TripCard />
-        <TripCard />
-        <TripCard />
-        <TripCard />
-        <TripCard />
-        <TripCard />
-        <TripCard />
+        {trips.map((trip) => (
+          <TripCard
+            key={trip.id}
+            id={trip.id}
+            title={trip.title}
+          />
+        ))}
       </div>
 
       <ButtonCircle />
--- frontend/src/utils/providers.tsx
+++ frontend/src/utils/providers.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ClerkProvider } from "@clerk/clerk-react";
 
+
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
