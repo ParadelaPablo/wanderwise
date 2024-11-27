bef95b8b9a9b0c3a2fe6027144b71ccdba41b218
Pablo Paradela <pablo.paradela@appliedtechnology.se>
2024-11-27 13:17:54 +0100

    footer and to do

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -4,26 +4,26 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = ({ setVisibleComponent }) => {
   return (
-    <footer className="bg-gray-800 text-white py-3 sticky bottom-0">
-      <div className="flex justify-around items-center">
-        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-24">
+    <footer className="bg-gray-800 text-white py-3 sticky bottom-0 w-full">
+      <div className="flex justify-around items-center gap-x-1">
+        <button className="flex-1 text-center px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
           Map
         </button>
         <button
           onClick={() => setVisibleComponent("Todo")}
-          className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 w-24"
+          className="flex-1 text-center px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
         >
           To Do
         </button>
         <button
           onClick={() => setVisibleComponent("ToPack")}
-          className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 w-24"
+          className="flex-1 text-center px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
         >
           To Pack
         </button>
         <button
           onClick={() => setVisibleComponent("Highlights")}
-          className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 w-24"
+          className="flex-1 text-center px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600"
         >
           Highlights
         </button>
