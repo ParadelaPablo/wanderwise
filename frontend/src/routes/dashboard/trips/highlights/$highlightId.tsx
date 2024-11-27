41f4ad459ca6fd40d1fd8ba087da3c35342733f0
Sebastian Österberg <sebastian.osterberg@appliedtechnology.se>
2024-11-27 11:00:52 +0100

    Updated the highlight component

--- frontend/src/routes/dashboard/trips/highlights/$highlightId.tsx
+++ frontend/src/routes/dashboard/trips/highlights/$highlightId.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute, useParams } from "@tanstack/react-router";
+import { Button } from "react-day-picker";
 
 export const Route = createFileRoute(
   "/dashboard/trips/highlights/$highlightId"
@@ -12,9 +13,31 @@ function RouteComponent() {
     select: (params) => params.highlightId,
   });
   return (
-    <div>
+    <div className="">
       <div className="btn">Back</div>
       <p>My note {highlightId}</p>
+      <div className="w-full flex flex-col justify-center items-center gap-2">
+        <p className="text-2xl">Title</p>
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered w-full max-w-xs"
+        />
+
+        <div className="w-screen p-3 flex flex-col justify-center items-center relative">
+          <div className="relative w-full">
+            <textarea
+              className="textarea textarea-bordered w-full resize-none h-72"
+              placeholder="Bio"
+            ></textarea>
+            <div className="absolute bottom-2 right-2 flex gap-2">
+              <button className="border w-6 h-6 border-black rounded"></button>
+              <button className="border w-6 h-6 border-black rounded"></button>
+              <button className="border w-6 h-6 border-black rounded"></button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
