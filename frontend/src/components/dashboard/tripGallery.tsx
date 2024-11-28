a10168d25c8b42f4ae5cb213493abfa69117f89a
Sebastian Österberg <sebastian.osterberg@appliedtechnology.se>
2024-11-28 10:15:32 +0100

    Implemented get trip function in the tripGallery for the dashboard

--- frontend/src/components/dashboard/tripGallery.tsx
+++ frontend/src/components/dashboard/tripGallery.tsx
@@ -2,31 +2,38 @@ import TripCard from "./tripCard";
 import ButtonCircle from "../buttons/buttonAddNewTripCircle";
 import { useRouter } from "@tanstack/react-router";
 import { useUser } from "@clerk/clerk-react";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
+const getTrips = async () => {
+  const response = await axios.get("http://localhost:8080/api/trips");
+  console.log(response.data);
+  return response.data;
+};
 
+const useTrips = () => {
+  return useQuery({
+    queryKey: ["trips"],
+    queryFn: getTrips,
+  });
+};
 
 const TripGallery = () => {
   const router = useRouter();
   const { user } = useUser();
+  const { data: trips = [], isLoading, isError, error } = useTrips();
 
-
-
-  const trips = [
-    { id: 1, title: "Trip to Milano" },
-    { id: 2, title: "Trip to Roma" },
-    { id: 3, title: "Trip to Firenze" },
-    { id: 4, title: "Trip to Napoli" },
-    { id: 5, title: "Trip to Venezia" },
-    { id: 6, title: "Trip to Milano" },
-
-  ];
+  if (isLoading) return <div>Loading trips...</div>;
+  if (isError) return <div>Error loading trips: {error.message}</div>;
 
   return (
     <div className="flex flex-col items-center justify-between min-h-screen relative">
+      <button onClick={getTrips}> This is fetch</button>
+
       <div className="text-center mt-5">
-      <h1 className="text-2xl font-bold">
+        <h1 className="text-2xl font-bold">
           Welcome to Your Dashboard
-          {user?.firstName && `, ${user.firstName}`} 
+          {user?.firstName && `, ${user.firstName}`}
         </h1>
         <button
           onClick={() => router.navigate({ to: "/dashboard/create" })}
@@ -38,11 +45,7 @@ const TripGallery = () => {
 
       <div className="flex flex-col w-80 items-center justify-center gap-5 mt-8 mb-20 border p-5">
         {trips.map((trip) => (
-          <TripCard
-            key={trip.id}
-            id={trip.id}
-            title={trip.title}
-          />
+          <TripCard key={trip.id} id={trip.id} title={trip.title} />
         ))}
       </div>
 
