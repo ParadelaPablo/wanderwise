f87287804346dd5222a3ad0dbbf436446efc2dff
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2024-11-28 16:43:55 +0100

    update settings

--- frontend/src/components/settings/settings.tsx
+++ frontend/src/components/settings/settings.tsx
@@ -2,11 +2,8 @@ import { UserProfile } from "@clerk/clerk-react";
 
 const SettingsPage = () => {
     return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-100 pr-25">
-            <div className="w-full max-w-2xl bg-white p-6 rounded-lg shadow-md">
-                <h1 className="text-2xl font-bold mb-4">Account Settings</h1>
-                <UserProfile />
-            </div>
+        <div className="flex pt-4 justify-center items-center min-h-screen bg-gray-100 pr-25">
+           <UserProfile />
         </div>
     );
 };
