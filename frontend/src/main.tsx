0f083c6161f43fe7e95dc14a62a30ea69abe7d23
Pablo Paradela <pablo.paradela@appliedtechnology.se>
2024-12-02 16:11:07 +0100

    add typografies and loading in dashboard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap');
+</style>
+
 
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
