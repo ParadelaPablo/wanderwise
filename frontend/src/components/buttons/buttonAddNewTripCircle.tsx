a9b6f41e644d8e43183fdd83187bcbc0b7d4f53f
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2024-12-04 11:05:20 +0100

    fix btn on the frontend dashboard

--- frontend/src/components/buttons/buttonAddNewTripCircle.tsx
+++ frontend/src/components/buttons/buttonAddNewTripCircle.tsx
@@ -1,27 +1,27 @@
 import { useNavigate } from "@tanstack/react-router";
 
 const ButtonCircle: React.FC = () => {
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-return (
-<button
-    onClick={() => navigate({ to: "/dashboard/create" })}
-    className="btn btn-circle bg-neutral text-primary hover:bg-teal-600 shadow-lg sticky bottom-5 mx-auto block flex items-center justify-center"
->
-    <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-8 w-8"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+  return (
+    <button
+      onClick={() => navigate({ to: "/dashboard/create" })}
+      className="btn btn-circle btn-primary text-gray-50 sticky bottom-5 mx-auto flex items-center justify-center"
     >
-    <path d="M12 5v14M5 12h14" />
-    </svg>
-</button>
-);
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-8 w-8"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M12 5v14M5 12h14" />
+      </svg>
+    </button>
+  );
 };
 
 export default ButtonCircle;
