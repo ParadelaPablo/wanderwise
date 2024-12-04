90564595f604c2cb70f8d26d7f4cd0ba466aba2e
Sebastian Österberg <sebastian.osterberg@appliedtechnology.se>
2024-12-04 11:47:16 +0100

    Updated the trip cards to use proper styling for themes

--- frontend/src/components/dashboard/tripCard.tsx
+++ frontend/src/components/dashboard/tripCard.tsx
@@ -12,23 +12,62 @@ const TripCard = ({ id, title, onDelete }: TripCardProps) => {
   return (
     <div>
       <div className="w-full relative">
-        <button
-          onClick={() => onDelete(id)}
-          className="btn-sm bg-neutral rounded text-primary m-2 absolute top-0 right-0 z-10"
-        >
-          X
-        </button>
+        {/* Dropdown positioned above the trip */}
+        <div className="dropdown dropdown-left absolute top-0 right-0 z-10">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost bg-transparent outline-transparent focus:outline-none focus:ring-0 outline-none text-2xl w-16 h-16 flex items-start justify-end"
+            style={{
+              outline: "none",
+              boxShadow: "none",
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-8 h-8"
+            >
+              <path d="M2 8a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM6.5 8a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM12.5 6.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z" />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-100 rounded-xl z-20 w-52 p-2 shadow outline outline-1"
+          >
+            <li>
+              <button
+                type="button"
+                className="btn btn-outline btn-info font-bold border-b border-gray-300 hover:bg-red-500 hover:text-white hover:border-red-500 transition-colors duration-200 ease-in-out"
+              >
+                Edit trip
+              </button>
+            </li>
 
+            <li>
+              <button
+                type="button"
+                onClick={() => onDelete(id)}
+                className="btn btn-outline btn-error font-bold border-b border-gray-300 hover:bg-red-500 hover:text-white hover:border-red-500 transition-colors duration-200 ease-in-out"
+              >
+                Delete trip
+              </button>
+            </li>
+          </ul>
+        </div>
+
+        {/* Trip button */}
         <button
           className="btn glass p-0 w-80 h-40 relative"
           onClick={() => router.navigate({ to: `/dashboard/trips/${id}` })}
         >
           <img
-            className="w-full h-full rounded-2xl object-cover"
+            className="w-full h-full rounded-xl object-cover"
             src="https://t3.ftcdn.net/jpg/03/04/88/18/360_F_304881889_yJ1S3butl9gVs0kMptYTU2N1EVmEJbz8.jpg"
             alt=""
           />
-          <p className="absolute top-0 left-0 w-full flex gap-2 bg-black bg-opacity-40 text-white px-4 py-2 rounded shadow-md rounded-t-2xl">
+          <p className="absolute top-0 left-0 w-full min-h-9 flex gap-2 bg-base-100/80 text-base-content px-4 py-2 rounded-t-xl shadow-md shadow-black/20 dark:shadow-gray-800">
             {title}
           </p>
         </button>
