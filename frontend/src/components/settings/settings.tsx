7df03489fb46d2c859e19f9036af02c720aaed2e
Pablo Paradela <pablo.paradela@appliedtechnology.se>
2024-12-05 13:49:54 +0100

    details2

--- frontend/src/components/settings/settings.tsx
+++ frontend/src/components/settings/settings.tsx
@@ -2,7 +2,7 @@ import { UserProfile } from "@clerk/clerk-react";
 
 const SettingsPage = () => {
     return (
-        <div className="flex pt-4 justify-center items-center min-h-screen bg-gray-100 pr-25">
+        <div className="flex pt-4 justify-center items-center bg-gray-100 pr-25 pb-5">
            <UserProfile />
         </div>
     );
