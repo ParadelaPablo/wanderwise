dc4a81b25d2428a87868046cd617dc3c627db2af
Sebastian Österberg <sebastian.osterberg@appliedtechnology.se>
2024-12-05 10:01:04 +0100

    Updated the footer with bigger buttons and new icons

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -15,14 +15,14 @@ const Footer: React.FC<FooterProps> = ({ setVisibleComponent }) => {
   return (
     <footer className="fixed bottom-0 w-full shadow-lg">
       <ul className="menu menu-horizontal bg-base-200 w-full justify-between">
-        <li className="shadow-sm rounded-2xl">
+        <li className="shadow-sm rounded-2xl ">
           <a
-            className={`rounded-2xl p-1 m-1 ${activeComponent === "Map" ? "text-indigo-700" : ""}`}
+            className={` flex flex-col w-20 rounded-2xl p-1 m-1 ${activeComponent === "Map" ? "text-indigo-700" : ""}`}
             onClick={() => handleClick("Map")}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4"
+              className="h-8 w-8 "
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -39,21 +39,21 @@ const Footer: React.FC<FooterProps> = ({ setVisibleComponent }) => {
         </li>
         <li className="shadow-sm rounded-2xl">
           <a
-            className={`rounded-2xl p-1 m-1  ${activeComponent === "Todo" ? "text-indigo-700" : ""}`}
+            className={`flex flex-col w-20 rounded-2xl p-1 m-1  ${activeComponent === "Todo" ? "text-indigo-700" : ""}`}
             onClick={() => handleClick("Todo")}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4"
               fill="none"
               viewBox="0 0 24 24"
+              stroke-width="1.5"
               stroke="currentColor"
+              className="size-8"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
               />
             </svg>
             Todo
@@ -61,21 +61,21 @@ const Footer: React.FC<FooterProps> = ({ setVisibleComponent }) => {
         </li>
         <li className="shadow-sm rounded-2xl">
           <a
-            className={`rounded-2xl p-1 m-1 ${activeComponent === "ToPack" ? "text-indigo-700" : ""}`}
+            className={`flex flex-col w-20 rounded-2xl p-1 m-1 ${activeComponent === "ToPack" ? "text-indigo-700" : ""}`}
             onClick={() => handleClick("ToPack")}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4"
               fill="none"
               viewBox="0 0 24 24"
+              stroke-width="1.5"
               stroke="currentColor"
+              className="size-8"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
               />
             </svg>
             ToPack
@@ -83,21 +83,26 @@ const Footer: React.FC<FooterProps> = ({ setVisibleComponent }) => {
         </li>
         <li className="shadow-sm rounded-2xl">
           <a
-            className={`rounded-2xl p-1 m-1 ${activeComponent === "Highlights" ? "text-indigo-700" : ""}`}
+            className={`flex flex-col w-20 rounded-2xl p-1 m-1 ${activeComponent === "Highlights" ? "text-indigo-700" : ""}`}
             onClick={() => handleClick("Highlights")}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4"
               fill="none"
               viewBox="0 0 24 24"
+              stroke-width="1.5"
               stroke="currentColor"
+              className="size-8"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2m0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
               />
             </svg>
             Highlights
