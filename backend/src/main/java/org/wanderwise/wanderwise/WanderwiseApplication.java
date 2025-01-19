94b3a830038a9e79b1db886dfbc41f4d144416d2
Pablo Paradela <pablo.paradela@appliedtechnology.se>
2025-01-19 19:57:52 +0100

    jdk 21 to jdk 17

--- backend/src/main/java/org/wanderwise/wanderwise/WanderwiseApplication.java
+++ backend/src/main/java/org/wanderwise/wanderwise/WanderwiseApplication.java
@@ -3,7 +3,6 @@ package org.wanderwise.wanderwise;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,7 +11,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "org.wanderwise.wanderwise.repository")
 public class WanderwiseApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("/app")
+				.load();
 
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
 		SpringApplication.run(WanderwiseApplication.class, args);
 	}
